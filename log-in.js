e5246ffd422c6b4961f247c52e367683aaa167ee
GhOssTYyy <jpandreiph@gmail.com>
2026-08-02 16:39:55 -0300

    modified:   log-in.js modified:   log-in_page.html
    Deixei a opção de ir página de registro de conta funcional, através do "a" dentro do log-in.js

--- log-in.js
+++ log-in.js
@@ -1,6 +1,8 @@
 const password_eye = document.getElementById("password-eye")
 const password_content = document.getElementById("password-content")
+const go_to_sign_html = document.getElementById("go-to-sign-html")
 
+//Deixar a senha visivel e mudar o ícone do olho.
 password_eye.addEventListener("click", function(){
 
     if (password_content.type === "password") {
@@ -15,5 +17,12 @@ password_eye.addEventListener("click", function(){
         password_eye.src = "icons/eye_closed.png"
     }
 
+
+})
+
+//Trocar para a página de registro de conta.
+go_to_sign_html.addEventListener("click", function(){
+
+    go_to_sign_html.href = "sign-in_page.html"
 
 })
