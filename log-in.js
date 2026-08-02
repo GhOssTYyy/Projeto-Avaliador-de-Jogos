203f69e598ef0542f43f6c88c878f3e0ccae1c14
GhOssTYyy <jpandreiph@gmail.com>
2026-08-02 16:57:31 -0300

    Algumas mudanças no visual e na qualidade dos arqivos de imagem.

--- log-in.js
+++ log-in.js
@@ -8,13 +8,13 @@ password_eye.addEventListener("click", function(){
     if (password_content.type === "password") {
 
         password_content.type = "text"
-        password_eye.src = "icons/eye_open.png"
+        password_eye.src = "icons/eye_open.svg"
 
 
     }else {
 
         password_content.type = "password"
-        password_eye.src = "icons/eye_closed.png"
+        password_eye.src = "icons/eye_closed.svg"
     }
 
 
