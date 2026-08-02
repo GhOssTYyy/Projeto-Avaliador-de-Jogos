d3c6dd784456dcefbb8448da91bab219b5062bab
GhOssTYyy <jpandreiph@gmail.com>
2026-08-02 17:42:56 -0300

    Adicionei a função de ver a senha no sign-in.html e criei um no javascript para o mesmo.

--- sign-in.js
+++ sign-in.js
@@ -1,28 +1,22 @@
 const password_eye = document.getElementById("password-eye")
 const password_content = document.getElementById("password-content")
-const go_to_sign_html = document.getElementById("go-to-sign-html")
+const arrow_back_page = document.getElementById("arrow-back-page")
 
-//Deixar a senha visivel e mudar o ícone do olho.
-password_eye.addEventListener("click", function(){
+arrow_back_page.addEventListener("click", function(){
 
-    if (password_content.type === "password") {
-
-        password_content.type = "text"
-        password_eye.src = "icons/eye_opened.svg"
-
-
-    }else {
-
-        password_content.type = "password"
-        password_eye.src = "icons/eye_closed.svg"
-    }
-
+    window.location.href = "log-in_page.html"
 
 })
 
-//Trocar para a página de registro de conta.
-go_to_sign_html.addEventListener("click", function(){
+password_eye.addEventListener("click", function(){
 
-    go_to_sign_html.href = "sign-in_page.html"
+    if (password_content.type === "password") {
+        
+        password_content.type = "text"
+        password_eye.src = "icons/eye_opened.svg"
 
+    }else {
+        password_content.type = "password"
+        password_eye.src = "icons/eye_closed.svg"
+    }
 })
